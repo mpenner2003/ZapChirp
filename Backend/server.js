e578bef95b14e9c1efa5937e75c88b971b2d0c4a
Gustavo <cotom@pdx.edu>
2024-05-29 13:47:51 -0700

    Backend (1st Draft).

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); //To parse the incoming requests with JSON payloads. (from req.body)
+
+app.use("/api/auth", authRoutes);
+
+//app.get("/", (req, res) => {
+    // root route http://localhost:5000/
+  //  res.send("Hello World!!");
+//});
+
+
+
+app.listen(PORT,() => {
+    connectToMongoDB();
+    console.log(`Server running on port ${PORT}`)
+});
+
+
