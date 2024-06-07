d69da3d7b30063a0904ecd49157f9a9fcaae3a37
Gustavo <cotom@pdx.edu>
2024-06-06 20:30:29 -0700

    Backend implementation draft #2, features group chat endpoints.

---
+++ Backend/controllers/groupChat.controller.js
@@ -0,0 +1,24 @@
+import GroupChat from '../models/groupChat.model.js';
+import User from '../models/user.model.js';
+
+
+export const createGroupChat = async (req, res) => {
+    const {groupName, members} = req.body;
+
+    if (!groupName || !members || members.length === 0){
+        return res.status(400).json({message: 'Group name and members are required.'});
+
+    }
+
+    try {
+        const users = await User.find({_id: { $in: members}});
+        if (users.length !== members.length) {
+            return res.status(400).json({message: 'One or more members do not exist.'});
+        }
+        const groupChat = new GroupChat({groupName, members});
+        await groupChat.save();
+        req.status(201).json(groupChat);
+    } catch(error) {
+        res.status(500).json({message: 'Error creating group chat', error: error.message});
+    }
+};
---
+++ Backend/routes/groupChat.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import {createGroupChat} from '../controllers/groupChat.controller.js';
+import protectRoute from '../middleware/protectRoute.js';
+
+
+const router = express.Router();
+
+router.post('/', protectRoute, createGroupChat);
+
+export default router;
